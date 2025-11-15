15e8e7b451312411aa29af9a89a49176416959c4
Rakib Islam <rakib13x@gmail.com>
2025-11-15 19:10:01 +0600

    pushing it again

--- app.js
+++ app.js
@@ -18,12 +18,12 @@ mongoose.connect(process.env.MONGO_URI, {
     },
     useNewUrlParser: true,
     useUnifiedTopology: true
-}, function(err) {
-    if (err) {
-        console.log("shaowaa er error!! " + err);
-    } else {
-        console.log("MongoDB Connection Successful");
-    }
+// }, function(err) {
+//     if (err) {
+//         console.log("shaowaa er error!! " + err);
+//     } else {
+//         console.log("MongoDB Connection Successful");
+//     }
 });
 
 
