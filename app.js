d8cd6429b9163ffaeb20ce72f3eb57f9e470a243
rakib13x <rakib13x@gmail.com>
2025-11-16 09:39:41 +0600

    installed mongoose new version

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const path = require('path');
 const express = require('express');
 const OS = require('os');
@@ -18,14 +19,12 @@ mongoose.connect(process.env.MONGO_URI, {
     },
     useNewUrlParser: true,
     useUnifiedTopology: true
-// }, function(err) {
-//     if (err) {
-//         console.log("shaowaa er error!! " + err);
-//     } else {
-//         console.log("MongoDB Connection Successful");
-//     }
-});
-
+}, function(err) {
+    if (err) {
+        console.log("error!! " + err)
+    } else {
+    }
+})
 
 var Schema = mongoose.Schema;
 
