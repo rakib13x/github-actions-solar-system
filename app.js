ffef35c3a588fa803d7ae45a1cba30487f861bfe
rakib13x <rakib13x@gmail.com>
2025-11-16 09:59:10 +0600

    trying to fix some code bug

--- app.js
+++ app.js
@@ -39,21 +39,20 @@ var dataSchema = new Schema({
 var planetModel = mongoose.model('planets', dataSchema);
 
 
+app.post('/planet', async function(req, res) {
+    try {
+        const planetData = await planetModel.findOne({ id: req.body.id });
+        if (!planetData) {
+            res.status(404).send({ error: "Planet not found. Select a number from 0 - 9." });
+        } else {
+            res.send(planetData);
+        }
+    } catch (err) {
+        res.status(500).send({ error: "Server error" });
+    }
+});
 
-app.post('/planet',   function(req, res) {
-   // console.log("Received Planet ID " + req.body.id)
-    planetModel.findOne({
-        id: req.body.id
-    }, function(err, planetData) {
-        if (err) {
-    res.status(500).send("Error in Planet Data");
-} else if (!planetData) {
-    res.status(404).send({error: "Planet not found"});
-} else {
-    res.send(planetData);
-}
-    })
-})
+
 
 app.get('/',   async (req, res) => {
     res.sendFile(path.join(__dirname, '/', 'index.html'));
